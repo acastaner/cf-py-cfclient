846d7e61472f32a17f8cff492b6b3f9add6c3fe0
Arnaud Castaner <arnaud.castaner@outlook.com>
2019-03-27 18:18:56 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='cyberfloodClient',
+     version='0.1',
+     scripts=['cyberfloodClient.py'] ,
+     author="Arnaud Castaner",
+     author_email="arnaud.castaner@outlook.com",
+     description="A CyberFlood REST API client",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/acastaner/cf-py-cfclient",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
